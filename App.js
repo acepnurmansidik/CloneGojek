7683e20c26ae1e71e63fb22a9b0be3eb586f5e53
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-25 08:32:56 +0700

    Gopay Features Section & clean code at styling

--- App.js
+++ App.js
@@ -14,11 +14,12 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollConatainer}>
-        <View style={styles.containerSeacrhBar}>
+        {/* Search Bar */}
+        <View style={[styles.containerSeacrhBar, styles.containerRow]}>
           <View style={{position: 'relative', flex: 1}}>
             <TextInput
               placeholder="What do you eat?"
-              style={styles.searchBar}
+              style={searchBarSection.searchBar}
             />
             <Image
               style={{position: 'absolute', top: 8, left: 8}}
@@ -32,49 +33,88 @@ export default function App() {
             />
           </View>
         </View>
-        <Text>sad</Text>
+        {/* Menu navigation */}
+        <View style={{marginTop: 18, borderRadius: 4, overflow: 'hidden'}}>
+          <View style={[styles.containerRow, menuSection.menuNavSection]}>
+            <Image source={require('./icons/gopay.png')} />
+            <Text style={{color: '#FFF', fontWeight: 'bold', fontSize: 17}}>
+              Rp. 50.000.000
+            </Text>
+          </View>
+          <View
+            style={[
+              styles.containerRow,
+              menuSection.menuNavSection,
+              menuSection.menuButton,
+            ]}>
+            <View style={navigation.itemNav}>
+              <View>
+                <Image source={require('./icons/pay.png')} />
+              </View>
+              <Text style={styles.txtGlobalStyle}>Pay</Text>
+            </View>
+            <View style={navigation.itemNav}>
+              <View>
+                <Image source={require('./icons/nearby.png')} />
+              </View>
+              <Text style={styles.txtGlobalStyle}>Nearby</Text>
+            </View>
+            <View style={navigation.itemNav}>
+              <View>
+                <Image source={require('./icons/topup.png')} />
+              </View>
+              <Text style={styles.txtGlobalStyle}>TopUp</Text>
+            </View>
+            <View style={navigation.itemNav}>
+              <View>
+                <Image source={require('./icons/more.png')} />
+              </View>
+              <Text style={styles.txtGlobalStyle}>More</Text>
+            </View>
+          </View>
+        </View>
       </ScrollView>
       <View style={styles.navContainer}>
-        <View style={styles.itemNav}>
-          <View style={styles.iconMenu}>
+        <View style={navigation.itemNav}>
+          <View style={{backgroundColor: 'white'}}>
             <Image
-              style={styles.iconImg}
+              style={navigation.iconImg}
               source={require('./icons/home-active.png')}
             />
           </View>
-          <Text style={[styles.textNav, styles.active]}>Home</Text>
+          <Text style={[styles.textNav, navigation.active]}>Home</Text>
         </View>
-        <View style={styles.itemNav}>
-          <View style={styles.iconMenu}>
+        <View style={navigation.itemNav}>
+          <View style={{backgroundColor: 'white'}}>
             <Image
-              style={styles.iconImg}
+              style={navigation.iconImg}
               source={require('./icons/order.png')}
             />
           </View>
           <Text style={styles.textNav}> Order</Text>
         </View>
-        <View style={styles.itemNav}>
-          <View style={styles.iconMenu}>
+        <View style={navigation.itemNav}>
+          <View style={{backgroundColor: 'white'}}>
             <Image
-              style={styles.iconImg}
+              style={navigation.iconImg}
               source={require('./icons/help.png')}
             />
           </View>
           <Text style={styles.textNav}>Help</Text>
         </View>
-        <View style={styles.itemNav}>
-          <View style={styles.iconMenu}>
+        <View style={navigation.itemNav}>
+          <View style={{backgroundColor: 'white'}}>
             <Image
-              style={styles.iconImg}
+              style={navigation.iconImg}
               source={require('./icons/inbox.png')}
             />
           </View>
           <Text style={styles.textNav}> Inbox</Text>
         </View>
-        <View style={styles.itemNav}>
-          <View style={styles.iconMenu}>
+        <View style={navigation.itemNav}>
+          <View style={{backgroundColor: 'white'}}>
             <Image
-              style={styles.iconImg}
+              style={navigation.iconImg}
               source={require('./icons/account.png')}
             />
           </View>
@@ -101,27 +141,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  itemNav: {
-    borderColor: 'black',
-    flex: 1,
-    alignItems: 'center',
+  containerSeacrhBar: {
+    paddingTop: 15,
   },
-  textNav: {
-    textAlign: 'center',
-    fontSize: 10,
-    color: '#545454',
-    marginTop: 4,
-  },
-  iconMenu: {
+  containerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: 'white',
   },
-  iconImg: {
-    height: 26,
-    width: 26,
+  txtGlobalStyle: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 13,
+    marginTop: 15,
   },
-  active: {
-    color: '#43aB4A',
-  },
+});
+
+const searchBarSection = StyleSheet.create({
   searchBar: {
     borderRadius: 25,
     borderColor: '#E8E8E8',
@@ -133,10 +170,38 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     marginRight: 18,
   },
-  containerSeacrhBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'white',
-    paddingTop: 15,
+});
+
+const menuSection = StyleSheet.create({
+  menuNavSection: {
+    backgroundColor: '#2C5FB8',
+    padding: 14,
+  },
+  menuButton: {
+    justifyContent: 'space-evenly',
+    paddingTop: 20,
+    paddingBottom: 14,
+    backgroundColor: '#2F65BD',
+  },
+});
+
+const navigation = StyleSheet.create({
+  itemNav: {
+    borderColor: 'black',
+    flex: 1,
+    alignItems: 'center',
+  },
+  textNav: {
+    textAlign: 'center',
+    fontSize: 10,
+    color: '#545454',
+    marginTop: 4,
+  },
+  active: {
+    color: '#43aB4A',
+  },
+  iconImg: {
+    height: 26,
+    width: 26,
   },
 });
